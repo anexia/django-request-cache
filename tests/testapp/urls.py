e1b0fd59efd622dbf3cf210c2432dcb34b3f3b9e
Harald Nezbeda <hnezbeda@anexia-it.com>
2023-10-13 17:22:05 +0200

    Test caching for pandas dataframe

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from testapp.views.sync import index_view
+from testapp.views.sync import index_view, pandas_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('pandas_dataframe/', pandas_view, name='pandas_dataframe'),
 ]
